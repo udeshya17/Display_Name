7949f4bf5de2141add10af8d825086e2eb3dd4a4
udeshya17 <udeshyasharma17@gmail.com>
2024-07-06 18:46:34 +0530

    Displayed the full name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [isClick, setIsClick] = useState(false);
+
+  const handleFirst = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleSecond = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(!isClick);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        First Name:
+        <input
+          type="text"
+          required
+          value={firstName}
+          onChange={handleFirst}
+        />
+        <br />
+        Last Name:
+        <input
+          type="text"
+          required
+          value={lastName}
+          onChange={handleSecond}
+        />
+        <br />
+        <button type="submit">Submit</button> 
+      </form>
+      <br />
+      {isClick ? `Full Name: ${firstName} ${lastName}` : ''}
     </div>
   );
 }
